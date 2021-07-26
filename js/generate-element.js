49f60692555259e9ffa267a06dbf29a0044c8a63
alexey.zharkov <alexey.zharkov@travelline.ru>
2021-07-26 16:43:02 +0300

    правки по защите

--- js/generate-element.js
+++ js/generate-element.js
@@ -1,13 +1,11 @@
 const generateElement = (templateSelector, elementUpdate, data) => {
   const template = document.querySelector(templateSelector).content.children[0];
   const element = template.cloneNode(true);
-  const message = document.createElement('div');
   if (elementUpdate) {
     elementUpdate(element, data);
   }
 
-  message.appendChild(element);
-  return message;
+  return element;
 };
 
 export {generateElement};
